5fe8f3ab742acdfba1c8b9f45295c65eeebfb963
Kyle Nguyen <kyle.nguyen304@gmail.com>
2021-10-27 03:14:31 -0400

    deployed api

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.send(database.users);
-// });
+app.get('/', (req, res) => {
+  res.send('it is working');
+});
 
 app.post('/signin', (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
