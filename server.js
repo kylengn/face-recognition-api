4a0a7b0d5cb7b02751f30df32375c19370e6a363
Kyle Nguyen <kyle.nguyen304@gmail.com>
2021-10-27 03:10:49 -0400

    deployed api

--- server.js
+++ server.js
@@ -55,8 +55,8 @@ app.post('/imageUrl', (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000 🚀');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port  ${process.env.PORT}🚀`);
 });
 
 /*
